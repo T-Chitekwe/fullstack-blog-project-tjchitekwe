2cf452eaf0e2b6ccac2e36f933d57277065516ac
T CHITEKWE <tjchitekwe@gmail.com>
2026-06-05 06:17:02 +0200

    Configure frontend for production API

--- backend/test.js
+++ backend/test.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb://tjchitekwe_db_user:ThxnderStorm@ac-sw5kuch-shard-00-00.mvax0d5.mongodb.net:27017,ac-sw5kuch-shard-00-01.mvax0d5.mongodb.net:27017,ac-sw5kuch-shard-00-02.mvax0d5.mongodb.net:27017/fullstackBlog?ssl=true&replicaSet=atlas-mnfwuf-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb://tjchitekwe_db_user:ThxnderStormKTN@ac-sw5kuch-shard-00-00.mvax0d5.mongodb.net:27017,ac-sw5kuch-shard-00-01.mvax0d5.mongodb.net:27017,ac-sw5kuch-shard-00-02.mvax0d5.mongodb.net:27017/fullstackBlog?ssl=true&replicaSet=atlas-mnfwuf-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("CONNECTED");
